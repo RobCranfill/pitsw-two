bfa0aa60022ac488cf82d1a6ed2e9e0227f4c8a5
Rob Cranfill <robcranfill@robcranfill.net>
2023-03-31 10:23:06 -0700

    Holy crap it kinda works!

--- pitsw.c
+++ pitsw.c
@@ -22,21 +22,16 @@
 #define PICO_PICO 0
 #define PICO_FEATHER 1
 
+#define N_LEDS  8
 #if PICO_PICO
 #define BUTTON_GPIO 15
-#endif
-
-#if PICO_FEATHER
-#define BUTTON_GPIO 25
-#endif
-
-#define N_LEDS  8
 // mapping from LED index to GPIO number
-#if PICO_PICO
 int led_mapping[] = {16, 17, 18, 19, 26, 22, 21, 20};
 #endif
 
 #if PICO_FEATHER
+#define BUTTON_GPIO 25
+// mapping from LED index to GPIO number
 int led_mapping[] = {6, 7, 8, 9, 10, 11, 12, 13};
 #endif
 
@@ -59,16 +54,13 @@ int led_data[N_COLS][N_LEDS] = {
 // flag for verifying thread startup
 #define WORKER_STARTED_FLAG 666
 
-// not sure why we'd check this:
-// #ifdef i2c_default
 
-
-/* Turn the given LED on for just a tad (and sleep after, for spacing?)
+/* Turn the given LED on for just a tad.
     I don't think this is useful.
 */
-void flash(int gpio, int on_time) {
+void flash(int gpio, int on_time_ms) {
     gpio_put(gpio, 1);
-    sleep_ms(on_time);
+    sleep_ms(on_time_ms);
     gpio_put(gpio, 0);
 }
 
@@ -91,12 +83,12 @@ void run_leds() {
             tight_loop_contents(); // needed??
         }
 
-        printf("FIRING!\n");
+        printf("firing LEDs and pausing %d ms\n", led_hold_time_ms);
 
-        // if (multicore_fifo_rvalid()) {
-        //     delay = (int)multicore_fifo_pop_blocking();
-        //     printf("    run_leds got updated delay = %d\n", delay);
-        //     }
+        if (multicore_fifo_rvalid()) {
+            led_hold_time_ms = (int)multicore_fifo_pop_blocking();
+            printf("    run_leds got updated led_hold_time_ms = %d\n", led_hold_time_ms);
+            }
 
         for (int col=0; col<N_COLS; col++) {
             for (int i=0; i<N_LEDS; i++) {
